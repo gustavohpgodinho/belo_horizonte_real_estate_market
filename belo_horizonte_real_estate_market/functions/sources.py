96c26d45f776a139ea160f1ce3731fe96f97863d
Gustavo Godinho <gustavohpgodinho@gmail.com>
2026-01-04 22:05:45 -0300

    ADD NEW DATA CODE

---
+++ belo_horizonte_real_estate_market/functions/sources.py
@@ -0,0 +1,39 @@
+
+DATASETS = {
+    "BHMAP_DATA": {
+    "cadastro_imobiliario": "https://geoservicos.pbh.gov.br/geoserver/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=ide_bhgeo:CADASTRO_IMOBILIARIO&srsName=EPSG:31983&outputFormat=csv",
+    "atividade_economica": "https://geoservicos.pbh.gov.br/geoserver/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=ide_bhgeo:ATIVIDADE_ECONOMICA&srsName=EPSG:31983&outputFormat=csv",
+    "atividade_economica_autonomos": "https://geoservicos.pbh.gov.br/geoserver/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=ide_bhgeo:ATIVIDADE_ECONOMICAS_AUTONOMOS&srsName=EPSG:31983&outputFormat=csv",
+    "conjuntos_habitacionais": "https://geoservicos.pbh.gov.br/geoserver/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=ide_bhgeo:CONJUNTO_HABITACIONAL&srsName=EPSG:31983&outputFormat=csv",
+    "enderecamento": "https://geoservicos.pbh.gov.br/geoserver/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=ide_bhgeo:ENDERECO&srsName=EPSG:31983&outputFormat=csv",
+    "logradouros": "https://geoservicos.pbh.gov.br/geoserver/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=ide_bhgeo:LOGRADOURO&srsName=EPSG:31983&outputFormat=csv",
+    "lote_ctm": "https://geoservicos.pbh.gov.br/geoserver/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=ide_bhgeo:LOTE_CTM&srsName=EPSG:31983&outputFormat=csv",
+    "projeto_edificacao_licenciado": "https://geoservicos.pbh.gov.br/geoserver/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=ide_bhgeo:PROJETO_EDIFICACAO_LICENCIADO&srsName=EPSG:31983&outputFormat=csv",
+    "zona_homogenea_iptu": "https://geoservicos.pbh.gov.br/geoserver/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=ide_bhgeo:ZONA_HOMOGENEA_IPTU&srsName=EPSG:31983&outputFormat=csv"
+    },
+
+    "API_DATASETS_ID": {"itbi": "0e13bf71-5355-47ce-8607-966413b08c0a",
+                        "enderecamento": "394da7d4-3e74-4a2d-8fb6-6cb4d65d3451",
+                        "cadastro_imobiliario_reg_pampulha": "e4f8fc31-df75-4f0b-9aab-f1abe1512d3f",
+                        "cadastro_imobiliario_reg_oeste": "da432bf7-dcd4-463d-b10a-ab4922c8e2b3",
+                        "cadastro_imobiliario_reg_norte": "df1cea37-92d0-4d52-917a-b1ed10bae4db",
+                        "cadastro_imobiliario_reg_noroeste": "afb79a2b-69b3-4482-82d5-df3025070fd0",
+                        "cadastro_imobiliario_reg_nordeste": "f7f03edc-39bc-4e17-aa94-a52d8bf56f04",
+                        "cadastro_imobiliario_reg_barreiro": "41c80bc4-e0e3-4d06-9547-a8a1051c9fb6",
+                        "cadastro_imobiliario_reg_leste": "f0745d2c-8b5c-4024-afb2-886c5de148e0",
+                        "cadastro_imobiliario_reg_hipercentro": "4000eb81-51fc-47d7-931e-445099e0f45a",
+                        "cadastro_imobiliario_reg_centrosul": "1ab07865-1a30-4e09-88c1-17d2183b2ea0",
+                        "cadastro_imobiliario_reg_vendanova": "3ddf7e20-812d-4e74-99cd-814c942cb8c3",
+                        "atividades_economicas_old": "3449df83-944b-4672-835d-d3e4a7bf7f48",
+                        "atividades_economicas": "33e9dcb1-126f-4cde-8c80-d1927a965430",
+                        "atividades_economicas_autonomos": "0e6890a3-6957-4d54-b6a6-0c77b97b1b88",
+                        "edificacoes_licenciadas": "602b0331-286a-4a59-a987-6f6f38f6ebec",
+                        "qtd_lancamentos_iptu_bairro": "177e1466-187d-47df-8638-1d02a060afc6",
+                        "baixa_construcoes": "aa138ff1-7229-4101-ba1c-399eee7de8be"},
+
+    "TIPO_CONSTRUTIVO": {"AP": "APARTAMENTO", "CA": "CASA", "SL": "SALA", "LV": "LOTE VAGO", "VC": "VAGA DE GARAGEM NAO RESIDENCIAL",
+                         "LJ": "LOJA", "VR": "VAGA DE GARAGEM RESIDENCIAL", "BA": "BARRACAO", "GP": "GALPAO",
+                         "AC": "APARTAMENTO COM OCUPACAO NAO RESIDENCIAL", "CC": "CASA COM OCUPACAO NAO RESIDENCIAL",
+                         "BC": "BARRACAO COM OCUPACAO NAO RESIDENCIAL", "VV": "VAGA DE GARAGEM NAO RESIDENCIAL"}   
+    
+    }
